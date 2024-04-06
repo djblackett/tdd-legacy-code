a0d3a9e992e01bf57d3c3b3851e6402520f266d3
David Andrea <davejandrea@gmail.com>
2024-04-06 15:31:02 -0300

    refactoring

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -40,24 +40,20 @@ export class Shop {
         // concert tickets have no value after date of concert
           if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
             item.quality = item.quality - item.quality;
-            return;
           }
 
       // Aged cheese increases in value
-        if (item.name === "Aged Brie" && item.quality < 50) {
+        else if (item.name === "Aged Brie" && item.quality < 50) {
           item.quality = item.quality + 1;
-          return;
         }
 
-      if (item.name === "Sulfuras, Hand of Ragnaros") {
-        return;
+      else if (item.name === "Sulfuras, Hand of Ragnaros") {
+
       }
 
-      if (item.quality > 0) {
+      else if (item.quality > 0) {
         // item.quality = item.quality - 1;
       }
-
-
     }
   }
 
